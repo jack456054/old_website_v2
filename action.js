1201733cd6fc0a3f029bb2162d0fb789a687cdb9
jack456054 <jack456054@hotmail.com>
2018-11-12 17:11:13 -0800

    Initialize bootstrap version

---
+++ action.js
@@ -0,0 +1,10 @@
+function dropdownMenu() {
+                var x = document.getElementById("dropdownClick");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                    /*change topnav to topnav.responsive*/
+                } 
+                else {
+                    x.className = "topnav";    
+                }
+            }
